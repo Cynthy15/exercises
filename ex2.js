9b6480921a76f6d45ddd9e2c7dcef4cae169d486
Cynthy Niganze <cynthynigaira@gmail.com>
2026-02-18 12:20:50 +0200

    adding exercise two

---
+++ ex2.js
@@ -0,0 +1,24 @@
+
+
+//Exercise two: Push, Pop, Shift, Unshift, Splice
+let invetory=[10,20,30];
+
+//a
+invetory.push(40);
+console.log(invetory);
+
+//b
+invetory.pop(40);
+console.log(invetory);
+
+//c
+invetory.unshift(0);
+console.log(invetory);
+
+//d
+invetory.shift(0);
+console.log(invetory);
+
+//e
+invetory.splice(1,1,25,35);
+console.log(invetory);
