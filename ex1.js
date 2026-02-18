d84d98a074df9258b8db24e5ee0979364d7929d5
Cynthy Niganze <cynthynigaira@gmail.com>
2026-02-18 12:19:29 +0200

    adding exercise one

---
+++ ex1.js
@@ -0,0 +1,12 @@
+// Exercise one: arrray initialization and property access
+const productCategories= ["Books", "Electronics", "Clothing", "Home & Kitchen", "Toys & Games"];
+//a
+console.log(productCategories[0]);
+//b
+console.log(productCategories[productCategories.length - 1]);
+//c
+console.log(productCategories.length);
+//d
+productCategories[1]= "Pens";
+//e
+console.log(productCategories);
