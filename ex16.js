5e7fa433856e3def70c8b5f8518b5a2798efe8f4
Cynthy Niganze <cynthynigaira@gmail.com>
2026-02-18 16:14:55 +0200

    adding exercise 16

--- ex16.js
+++ ex16.js
@@ -13,9 +13,7 @@ let sum = 0;
 for (let i = 0; i < gamesBoard.length; i++) {
   for (let j = 0; j < gamesBoard[i].length; j++) {
     console.log(gamesBoard[i][j]); 
-    sum += gamesBoard[i][j];     
+    sum += gamesBoard[i][j];
   }
 }
-
-// Print total sum
 console.log("Sum of all elements:", sum);
